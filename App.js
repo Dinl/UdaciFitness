2186dd272fd09150e2c4574bd0d2f5edbd93e024
Daniel Gomez <dinl_@hotmail.com>
2017-12-11 23:23:58 -0500

    Parte de botones

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View >
-        {getMetricMetaInfo('bike').getIcon()}
+        <AddEntry />
       </View>
     );
   }
