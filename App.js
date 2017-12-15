ae36ec25d8787e9ee3abd6142c79d91b5b298cfb
Daniel Gomez <dh.gomez@transportsystems.co>
2017-12-14 20:53:36 -0500

    se añade sliders

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import AddEntry from './components/AddEntry'
 import { getMetricMetaInfo } from './utils/helpers';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View >
-        <AddEntry />
+      <View style={styles.containe}>
+		<AddEntry />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginLeft: 10,
+		marginRight: 10,
+		alignItems: "stretch",
+		justifyContent: "center"
+	}
+})
